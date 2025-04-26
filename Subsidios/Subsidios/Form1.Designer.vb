c3b25fade3d38a6d8a91def0b6e964d19516d675
D4niel2154 <danielarcinieganevarez@gmail.com>
2025-04-26 17:41:25 -0600

    Add files via upload

---
+++ Subsidios/Subsidios/Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtCodigoBarras = New TextBox()
+        SuspendLayout()
+        ' 
+        ' txtCodigoBarras
+        ' 
+        txtCodigoBarras.Location = New Point(432, 122)
+        txtCodigoBarras.Name = "txtCodigoBarras"
+        txtCodigoBarras.Size = New Size(231, 27)
+        txtCodigoBarras.TabIndex = 0
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtCodigoBarras)
+        Name = "Form1"
+        Text = "Subsidios"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtCodigoBarras As TextBox
+
+End Class
---
+++ Subsidios/Subsidios/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtCodigoBarras_KeyDown(sender As Object, e As KeyEventArgs) Handles txtCodigoBarras.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            ' Leer el código de barras y buscar el producto en la base de datos
+            Dim codigoBarras As String = txtCodigoBarras.Text
+            ' Llamar a la función para buscar el producto BuscarProducto(codigoBarras)
+        End If
+    End Sub
+End Class
